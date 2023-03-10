3774d9eb7bb6c08c2de74c76a319030228a02bfe
Ghimirevivek <stephengmre@gmail.com>
2023-03-10 22:28:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from 'react'
+import '../styles/App.css'
 const App = () => {
-//code here
+  //code here
+  const [value, setValue] = useState('')
+  const changeText = () => {
+    const para = document.querySelector('#text')
+    para.innerText = `Hello my name is ${value} and I study at Newton School`
+  }
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id="input" onChange={(e) => setValue(e.target.value)}></input>
+      <button id="button" onClick={changeText}>
+        Click
+      </button>
+      <p id="text"> Hello my name is ____ and I study at Newton School</p>
     </div>
   )
 }
 
-
-export default App;
+export default App
